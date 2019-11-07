32190927ff5d670a81f3085754e78467b5932484
DAM222 <uxiacastro@gmail.com>
2019-11-07 10:36:05 +0100

    7/11/2019  Acciones sobre la lista

--- app/src/main/java/com/example/dam222/mislugaresuxiarcapp/presentacion/TipoLugarActivity.java
+++ app/src/main/java/com/example/dam222/mislugaresuxiarcapp/presentacion/TipoLugarActivity.java
@@ -1,7 +1,10 @@
 package com.example.dam222.mislugaresuxiarcapp.presentacion;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
 import com.example.dam222.mislugaresuxiarcapp.R;
 import com.example.dam222.mislugaresuxiarcapp.casouso.CasoUsoLugares;
@@ -25,6 +28,15 @@ public class TipoLugarActivity extends ListActivity {
         usoLugares=new CasoUsoLugares(this, ((Aplicacion) getApplication()).getLugares());
         adaptador= new AdaptadorLugares(this,usoLugares);
         setListAdapter(adaptador);
-    }
+    }//fin on create
+
+    @Override protected void onListItemClick(ListView listView, View vista, int
+            position, long id) {
+        super.onListItemClick(listView, vista, position, id);
+        Intent i = new Intent(this, VistaLugarActivity.class);
+        i.putExtra("pos",(int) id);
+        startActivity(i);
+    }//fin onlisitemclick
+
 
 }
